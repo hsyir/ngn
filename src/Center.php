c7dcf5cd327742081c0c01b6d921a549153ea58c
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2021-03-10 00:14:52 +0330

    work on drivers

--- src/Center.php
+++ src/Center.php
@@ -6,5 +6,20 @@ namespace Hsy\Ngn;
 
 class Center
 {
+    public function search($preNumber, $midNumber, $number)
+    {
+        $driver = $this->makeQueryDriver($preNumber, $midNumber);
 
+        $response = $driver->query($number);
+
+        return $response;
+
+    }
+
+    private function makeQueryDriver($preNumber, $midNumber)
+    {
+        $config = $this->getConfig($preNumber, $midNumber);
+
+        return new QueryDriver($config);
+    }
 }
