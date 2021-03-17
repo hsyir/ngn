d036fa1f543d2a97a5921e285dec6181bbe6a4eb
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2021-03-17 18:12:54 +0330

    Update SearchResponse.php

--- src/SearchResponse.php
+++ src/SearchResponse.php
@@ -11,8 +11,11 @@ class SearchResponse
     public $number;
     public $category;
     public $price;
+    public $status;
 
-
-
+    const STATUS_FREE="green";
+    const STATUS_REGISTERED="red";
+    const STATUS_RESERVED="orange";
+    const STATUS_UNKNOWN="gray";
 
 }
